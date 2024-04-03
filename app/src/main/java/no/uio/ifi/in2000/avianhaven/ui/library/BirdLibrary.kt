3bf830ead1589d8ca424056b265090a3b92e2588
tinaia <tinaia@ulrik.uio.no>
2024-04-03 17:08:43 +0200

    Test

--- app/src/main/java/no/uio/ifi/in2000/avianhaven/ui/library/BirdLibrary.kt
+++ app/src/main/java/no/uio/ifi/in2000/avianhaven/ui/library/BirdLibrary.kt
@@ -1,9 +1,10 @@
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun BirdLibrary() {
+    Text(text = "Test")
     LazyColumn {
-
     }
 }
