9b150bd343737c04307c340b632d5042087460c6
tinaia <tinaia@ulrik.uio.no>
2024-04-03 18:02:13 +0200

    Test

---
+++ app/src/main/java/no/uio/ifi/in2000/avianhaven/ui/home/NyTest.kt
@@ -0,0 +1,9 @@
+package no.uio.ifi.in2000.avianhaven.ui.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NyTest() {
+    Text(text = "Hei")
+}
